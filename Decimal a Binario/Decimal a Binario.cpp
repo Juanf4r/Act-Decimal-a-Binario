611e3376075667fb2efb2e1aa95c6291c53cac97
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2022-11-15 22:32:48 -0600

    Updated

--- Decimal a Binario/Decimal a Binario.cpp
+++ Decimal a Binario/Decimal a Binario.cpp
@@ -8,15 +8,18 @@ int main()
 {
     int num_decimal;
     int num_binario[8] = {};
+    //PRESENTACION
     std::cout << "--------------CONVERTIDOR DECIMAL A BINARIO--------------\n";
     std::cout << "\nDigite el Numero Decimal a convertir: ";
     std::cin >> num_decimal; std::cout << std::endl;
 
+    //CONVERSION
     for (int i = 0; i < 8; i++)
     {
         num_binario[i] = num_decimal % 2;
         num_decimal /= 2;
     }
+    //MOSTRAR CONVERSION
     std::cout << "El Numero Binario es: ";
     for (int i = 7; i >= 0; i--)
     {
